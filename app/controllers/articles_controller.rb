b6b4b520b46e49b16c89ed2b743f065aac683481
Deepti Shukla <deeptishukla24@gmail.com>
2018-10-23 02:55:23 +0000

    Added edit, show and update actions in controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,13 +3,26 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end 
   
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end 
+  
   def create
     @article = Article.new(article_params)
     if @article.save
       flash[:notice] = "Article was successfully created"
       redirect_to article_path(@article)
     else
-      
       render 'new'
     end
   end
